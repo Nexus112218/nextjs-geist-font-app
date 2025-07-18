680dc291b7da8ca8e62179013d4b89f653e7657c
Nexus112218 <147884750+Nexus112218@users.noreply.github.com>
2025-07-18 17:48:57 +0800

    Add .next/static/chunks/src_app_poi_[id]_page_tsx_11f632dd._.js 1752832136347

---
+++ .next/static/chunks/src_app_poi_[id]_page_tsx_11f632dd._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5e775555._.js",
+  "static/chunks/node_modules_7afb3fbd._.js"
+],
+    source: "dynamic"
+});
